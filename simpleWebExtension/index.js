1881af5342771092cfcde2b624b9ba6ce98e234b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-02-15 18:11:51 +0100

    Update index.js

--- simpleWebExtension/index.js
+++ simpleWebExtension/index.js
@@ -1,14 +1,1 @@
-let inputBtn = document.getElementById("input-Btn")
-let inputEl = document.getElementById("input-el")
-let myLeads = ["www.google.com", "www.vk.com", "www.binance.com"]
-const ulEl = document.getElementById("ul-el")
 
-console.log(ulEl)
-
-inputBtn = addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-})
-
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-}
