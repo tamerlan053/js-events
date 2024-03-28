ddb168e69bab3ba06900faf720a8cded99bd37c3
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-03-28 22:24:04 +0100

    Update index.js
    small syntax correction

--- peopleCounter/index.js
+++ peopleCounter/index.js
@@ -1,20 +1,19 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
  
 
 let count = 0;
 
 
 function increment () {
-    count = count + 1 
-    countEl.innerText = count
-    
+    count = count + 1 ;
+    countEl.innerText = count;
 }
 
 function save () {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
     
-    countEl.innerText = 0
-    count = 0
+    countEl.innerText = 0;
+    count = 0;
 }
